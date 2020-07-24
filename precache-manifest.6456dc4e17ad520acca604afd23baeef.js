b6d8f23a3b874c850fe13e9bbc2697ecf6f21731
Daniel Beckley <tomiwabeckley@gmail.com>
2020-07-24 01:15:20 +0100

    Updates

---
+++ precache-manifest.6456dc4e17ad520acca604afd23baeef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbf9ed33f7ded957ff3d002ef9b396b9",
+    "url": "/coachlivinglegend/reactTodoAppWithRedux/index.html"
+  },
+  {
+    "revision": "df616356bc8795f6c291",
+    "url": "/coachlivinglegend/reactTodoAppWithRedux/static/css/main.d93d0046.chunk.css"
+  },
+  {
+    "revision": "51292b828a495ce7c773",
+    "url": "/coachlivinglegend/reactTodoAppWithRedux/static/js/2.ed20a886.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coachlivinglegend/reactTodoAppWithRedux/static/js/2.ed20a886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df616356bc8795f6c291",
+    "url": "/coachlivinglegend/reactTodoAppWithRedux/static/js/main.f0117ee3.chunk.js"
+  },
+  {
+    "revision": "6cbcf83a3bc70c62b831",
+    "url": "/coachlivinglegend/reactTodoAppWithRedux/static/js/runtime-main.d7838c7b.js"
+  }
+]);
